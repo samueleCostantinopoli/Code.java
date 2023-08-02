5f29768270b0e736c5f918f553b56ff3e1fb9290
samueleCostantinopoli <samuele2552@gmail.com>
2023-08-02 17:06:59 +0200

    Start

---
+++ src/main/java/com/example/fitnesshelp/ControllerGrafico2BuyPreviewWorkoutPlan.java
@@ -0,0 +1,222 @@
+package com.example.fitnesshelp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControllerGrafico2BuyPreviewWorkoutPlan {
+
+    @FXML
+    private Label NameSurnameAge1;
+
+    @FXML
+    private AnchorPane WorkoutPlan2Pane;
+
+    @FXML
+    private Hyperlink accountHyperlink;
+
+    @FXML
+    private Hyperlink buyWorkoutPlanHyperlink;
+
+    @FXML
+    private Hyperlink buyWorkoutPlanHyperlink1;
+
+    @FXML
+    private AnchorPane dataWorkoutPlan1Pane;
+
+    @FXML
+    private Hyperlink findAGymNearMeHyperlink;
+
+    @FXML
+    private Hyperlink homeHyperlink;
+
+    @FXML
+    private ImageView homeImageView;
+
+    @FXML
+    private Button infoWorkoutPlan2Button;
+
+    @FXML
+    private Label insertPrerequisites;
+
+    @FXML
+    private Label insertPreview;
+
+    @FXML
+    private Hyperlink logoutHyperlink;
+
+    @FXML
+    private ImageView logoutImageView;
+
+    @FXML
+    private Hyperlink myTrainingDiaryHyperlink;
+
+    @FXML
+    private Hyperlink myWorkoutPlanHyperlink;
+
+    @FXML
+    private Label nameSurname;
+
+    @FXML
+    private Button nextBuyWorkoutPlanButton;
+
+    @FXML
+    private Button pageOneBuyWorkoutPlanButton;
+
+    @FXML
+    private Button pageTreeBuyWorkoutPlanButton;
+
+    @FXML
+    private Button pageTwoBuyWorkoutPlanButton;
+
+    @FXML
+    private Hyperlink personalTrainerHyperlink1;
+
+    @FXML
+    private Label prerequisites;
+
+    @FXML
+    private Label preview;
+
+    @FXML
+    private Button priceWorkoutPlan2Button;
+
+    @FXML
+    private ImageView profileImage;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    private Label welocme;
+
+    private Stage stage;
+    private Scene scene;
+    private FXMLLoader root;
+
+    @FXML
+    void clickedOnAccountHyperlink(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("account.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void clickedOnButtonInfoWorkoutPlan(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("buyInfoWorkoutPlan.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void clickedOnButtonNextBuyWorkoutPlan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickedOnButtonPageOneBuyWorkoutPlan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickedOnButtonPageTreeBuyWorkoutPlan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickedOnButtonPageTwoBuyWorkoutPlan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickedOnButtonPriceWorkoutPlan(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clickedOnBuyWorkoutPlanHyperlink(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("buyWorkoutPlan.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void clickedOnBuyWorkoutPlanHyperlink1(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("buyWorkoutPlan.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void clickedOnFindAGymNearMeHyperlink(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("findAGymNearMe.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void clickedOnHomeHyperlink(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("homePage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void clickedOnLogoutHyperlink(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("login.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void clickedOnMyTrainingDiaryHyperlink(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("trainingDiary.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void clickedOnMyWorkoutPlanHyperlink(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("myWorkoutPlan.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void clickedOnPersonalTrainerHyperlink(ActionEvent event) throws IOException {
+        root = new FXMLLoader(getClass().getResource("personalTrainer.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root.load());
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
