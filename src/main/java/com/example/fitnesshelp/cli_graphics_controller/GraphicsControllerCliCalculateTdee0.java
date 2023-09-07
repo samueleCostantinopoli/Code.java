47d328b61e2968b46c8814a3528fe1afa59fdc70
shenron221B <120735073+shenron221B@users.noreply.github.com>
2023-09-07 12:35:29 +0200

    prova code smell con printer al posto di sout

--- src/main/java/com/example/fitnesshelp/cli_graphics_controller/GraphicsControllerCliCalculateTdee0.java
+++ src/main/java/com/example/fitnesshelp/cli_graphics_controller/GraphicsControllerCliCalculateTdee0.java
@@ -3,6 +3,7 @@ package com.example.fitnesshelp.cli_graphics_controller;
 import com.example.fitnesshelp.application_controllers.ApplicationControllerCalculateTdee;
 import com.example.fitnesshelp.entities.Tdee;
 import com.example.fitnesshelp.exception.TdeeRemoveException;
+import com.example.fitnesshelp.utils.Printer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,10 +19,10 @@ public class GraphicsControllerCliCalculateTdee0 {
         while (exit) {
             printTdeeList(tdeeList);
 
-            System.out.println("\nSelect an option:");
-            System.out.println("1: Return to home");
-            System.out.println("2: Calculate new tdee");
-            System.out.println("3: Delete a tdee");
+            Printer.print("\nSelect an option:");
+            Printer.print("1: Return to home");
+            Printer.print("2: Calculate new tdee");
+            Printer.print("3: Delete a tdee");
 
             String userInput = getUserInput();
 
@@ -36,7 +37,7 @@ public class GraphicsControllerCliCalculateTdee0 {
                     exit = false;
                 }
                 case "3" -> deleteTdee(tdeeList);
-                default -> System.out.println("Invalid option.");
+                default -> Printer.print("Invalid option.");
             }
         }
     }
@@ -45,13 +46,13 @@ public class GraphicsControllerCliCalculateTdee0 {
         // this method print all tdee of user
         int index = 0;
         for (Tdee tdee : tdeeList) {
-            System.out.println("\nTDEE #" + index);
-            System.out.println("Kcal: " + tdee.getKcal());
-            System.out.println("Protein: " + tdee.getPro());
-            System.out.println("Fat: " + tdee.getFat());
-            System.out.println("Carb: " + tdee.getCarb());
-            System.out.println("Target: " + tdee.getTarget());
-            System.out.println("Quantity: " + tdee.getQuantity());
+            Printer.print("\nTDEE #" + index);
+            Printer.print("Kcal: " + tdee.getKcal());
+            Printer.print("Protein: " + tdee.getPro());
+            Printer.print("Fat: " + tdee.getFat());
+            Printer.print("Carb: " + tdee.getCarb());
+            Printer.print("Target: " + tdee.getTarget());
+            Printer.print("Quantity: " + tdee.getQuantity());
 
             index++;
         }
@@ -67,18 +68,18 @@ public class GraphicsControllerCliCalculateTdee0 {
         // to delete a tdee
         // command to insert: 'delete #tdee'
         ApplicationControllerCalculateTdee applicationControllerCalculateTdee = new ApplicationControllerCalculateTdee();
-        System.out.println("Type 'delete <TDEE number>' to delete a TDEE");
+        Printer.print("Type 'delete <TDEE number>' to delete a TDEE");
         String deleteCommand = getUserInput();
 
         if (!isValidDeleteCommand(deleteCommand)) {
-            System.out.println("Invalid command.");
+            Printer.print("Invalid command.");
             return;
         }
 
         int tdeeToDelete = extractTdeeToDelete(deleteCommand);
 
         if (tdeeToDelete < 0 || tdeeToDelete >= tdeeList.size()) {
-            System.out.println("Invalid TDEE number.");
+            Printer.print("Invalid TDEE number.");
             return;
         }
 
@@ -87,7 +88,7 @@ public class GraphicsControllerCliCalculateTdee0 {
 
         try {
             applicationControllerCalculateTdee.removeTdee(tdee);
-            System.out.println("tdee deleted\n");
+            Printer.print("tdee deleted\n");
         } catch (SQLException e) {
             // remove failed
             throw new TdeeRemoveException("Error while removing TDEE", e);
